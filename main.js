8cb4622784e49fc3e91cd0ac5199f16cce5dc4b6
HuyVu0901 <candycorman@gmail.com>
2023-06-30 21:08:59 +0700

    finished dropdown

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+/// Setup dropdown
 
-// setupCounter(document.querySelector('#counter'))
+document.querySelectorAll('.dropdown-toggle').forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault();
+      event.stopPropagation();
+      $(event.target).siblings('.dropdown-menu').toggle();
+    });
+  });
+
+document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', event => {
+      const selectedValue = event.target.textContent;
+      document.querySelector("#dropdown").textContent = selectedValue; 
+      console.log('Selected value:', selectedValue);
+    });
+  });
