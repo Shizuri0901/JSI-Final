37da2e7db39ae15137fc5ac15c429576f0398317
HuyVu0901 <candycorman@gmail.com>
2023-06-30 21:36:14 +0700

    dropdown fixed

--- main.js
+++ main.js
@@ -4,18 +4,51 @@ import viteLogo from '/vite.svg'
 
 /// Setup dropdown
 
-document.querySelectorAll('.dropdown-toggle').forEach(item => {
-    item.addEventListener('click', event => {
-      event.preventDefault();
-      event.stopPropagation();
-      $(event.target).siblings('.dropdown-menu').toggle();
-    });
-  });
+// Get dropdown toggle buttons and dropdown menus
+var dropdownToggle1 = document.querySelector('#dropdown-toggle-1');
+var dropdownToggle2 = document.querySelector('#dropdown-toggle-2');
+var dropdownMenu1 = document.querySelector('#dropdown-menu-1');
+var dropdownMenu2 = document.querySelector('#dropdown-menu-2');
 
-document.querySelectorAll('.dropdown-item').forEach(item => {
-    item.addEventListener('click', event => {
-      const selectedValue = event.target.textContent;
-      document.querySelector("#dropdown").textContent = selectedValue; 
-      console.log('Selected value:', selectedValue);
-    });
-  });
+// Variables to store selected values
+var selectedOption1 = '';
+var selectedOption2 = '';
+
+// Toggle visibility of dropdown menu 1
+dropdownToggle1.addEventListener('click', function() {
+  dropdownMenu1.classList.toggle('show');
+});
+
+// Toggle visibility of dropdown menu 2
+dropdownToggle2.addEventListener('click', function() {
+  dropdownMenu2.classList.toggle('show');
+});
+
+// Close dropdown menus when clicking outside of them
+document.addEventListener('click', function(event) {
+  var target = event.target;
+  if (target !== dropdownToggle1 && !dropdownToggle1.contains(target)) {
+    dropdownMenu1.classList.remove('show');
+  }
+  if (target !== dropdownToggle2 && !dropdownToggle2.contains(target)) {
+    dropdownMenu2.classList.remove('show');
+  }
+});
+
+// Handle click on dropdown menu 1 items
+dropdownMenu1.addEventListener('click', function(event) {
+  var target = event.target;
+  if (target.classList.contains('dropdown-item')) {
+    selectedOption1 = target.textContent;
+    console.log('Selected option 1:', selectedOption1);
+  }
+});
+
+// Handle click on dropdown menu 2 items
+dropdownMenu2.addEventListener('click', function(event) {
+  var target = event.target;
+  if (target.classList.contains('dropdown-item')) {
+    selectedOption2 = target.textContent;
+    console.log('Selected option 2:', selectedOption2);
+  }
+});
